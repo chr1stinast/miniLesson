40356b0e77da7bb90eb7d3fc420c73f43c8957ef
Your Name <you@example.com>
2021-12-02 10:37:27 -0500

    finally done with the text

--- final/script.js
+++ final/script.js
@@ -12,6 +12,7 @@ function isInViewport(el) {
 
 const box = document.querySelector('h2');
 const box2 = document.querySelector('#whatIs');
+const box3 = document.querySelector('#options');
 document.addEventListener('scroll', function () {
     if(isInViewport(box)) {
         box.classList.add("typewriter-text");
@@ -29,4 +30,21 @@ document.addEventListener('scroll', function () {
     }
 }, {
     passive: true
+});
+currentText = ["Effects", "Event Handling", "DOM Manipulation"];
+currentColor= ["#f92672", "#a6e22e", "#e6db74"];
+let i = 0;
+box3.addEventListener('animationend', () => {
+    setTimeout(function(){
+        i++;
+        if (i == 3) {
+            i = 0;
+        }
+        box3.style.animationTimingFunction = "steps(currentText[i].length, end)";
+        box3.style.color = currentColor[i]
+        box3.classList.remove("typewriter-text");
+        void box3.offsetWidth;
+        box3.classList.add("typewriter-text");
+        box3.textContent = currentText[i];
+    }, 1000); 
 });
